70406e51234aef7a7d48e55af777cd040c1956c3
DisfriGus <dio.siregar03@gmail.com>
2022-08-21 00:42:12 +0700

    main

--- public/script.js
+++ public/script.js
@@ -63,7 +63,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
+  let slides = document.getElementsByClassName("Slide");
   let dots = document.getElementsByClassName("dot");
   if (n > slides.length) { slideIndex = 1 }
   if (n < 1) { slideIndex = slides.length }
